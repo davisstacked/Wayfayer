3a5b0cb04c164f638d4c5f63f5cce116b7b07545
davisstacked <brettzvi@gmail.com>
2020-08-05 12:07:28 -0700

    created and migrated models

--- main_app/models.py
+++ main_app/models.py
@@ -1,4 +1,23 @@
 from django.db import models
 from django.contrib.auth.models import User
 
-# Create your models here.
+class City(models.Model):
+    name = models.CharField(max_length=150)
+    country = models.CharField(max_length=150)
+    image = models.URLField(max_length=350)
+
+
+class Profile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    city = models.ForeignKey(City, on_delete=models.CASCADE)
+    join_date = models.DateField(auto_now=False, auto_now_add=True)
+    image = models.URLField(max_length=350, default='https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460_960_720.png')
+
+
+class Post(models.Model):
+    city = models.ForeignKey(City, on_delete=models.CASCADE)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    title = models.CharField(max_length=200)
+    blurb = models.TextField(max_length=500)
+    post_date = models.DateField(auto_now=False, auto_now_add=True)
+    edit_date = models.DateField(auto_now=True)
