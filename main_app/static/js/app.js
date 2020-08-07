847b0b1a8b0f6db2a5d8f92cd8b1b660045e108f
xTonyYu <xtonyyu@gmail.com>
2020-08-07 15:21:49 -0700

    complete post count by city

--- main_app/static/js/app.js
+++ main_app/static/js/app.js
@@ -1,29 +1,38 @@
 console.log("WayFAYer")
 
-const closeBox = document.querySelector('.close-box');
-const dialogueContainer = document.querySelector('.dialogueContainer')
-const profileCity = document.querySelectorAll('.profile-city')
-const profilePost = document.querySelectorAll('.profile-post')
-const cities = document.querySelector('.cities-click')
+const closeBox = document.querySelector('.close-box') || null;
+const dialogueContainer = document.querySelector('.dialogueContainer') || null
+const profileCity = document.querySelectorAll('.profile-city') || null
+const profilePost = document.querySelectorAll('.profile-post') || null
+const cities = document.querySelector('.cities-click') || null // profile and show-city pages
+const postHeader = document.querySelector('.post-header') || null // profile and show-city pages
 
-
-cities.addEventListener('click', e => {
-    profileCity.forEach(city => city.style.background = 'gray')
-    profilePost.forEach(post => {
-        post.hidden = false;
+if (cities !== null ) {
+    cities.addEventListener('click', e => {
+        profileCity.forEach(city => city.style.background = 'gray')
+        let count = 0;
+        profilePost.forEach(post => {
+            post.hidden = false;
+            count++
+        })
+        postHeader.innerText = `Posts (${count})`
     })
-})
+}
 
 profileCity.forEach(city => city.addEventListener('click', e => {
     profileCity.forEach(city => city.style.background = 'gray')
     city.style.background = 'orange'
     const chosenCity = city.dataset.city
+    let count = 0;
     profilePost.forEach(post => {
         post.hidden = false;
         if (post.dataset.city !== chosenCity) {
             post.hidden = true
+        } else {
+            count++
         }
     })
+    postHeader.innerText = `Posts (${count})`
 })
 )
 
@@ -31,3 +40,5 @@ closeBox.addEventListener('click', e => {
     dialogueContainer.classList.add('hidden')
     window.history.back()
 })
+
+
